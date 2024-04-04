cb67d294b4a1d9c0d5737ff15ce291210e89b1be
Kaiyang Wu <self@origincode.me>
2024-04-03 23:10:51 -0700

    feat(cabinets): impl leave
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- src/handlers/cabinets.rs
+++ src/handlers/cabinets.rs
@@ -21,7 +21,8 @@ pub(crate) fn cabinets_config(cfg: &mut web::ServiceConfig) {
         .service(players)
         .service(upcoming)
         .service(next)
-        .service(join);
+        .service(join)
+        .service(leave);
 }
 
 /// Get the cabinet info with `cabinet_id` `GET /cabinets/{cabinet_id}`
@@ -86,11 +87,11 @@ LIMIT $2
 }
 
 /// List upcoming N players in the queue, and remove them from the queue
-/// `GET /cabinets/{cabinet_id}/next?n=N`
-#[get("{cabinet_id}/next")]
+/// `POST /cabinets/{cabinet_id}/next - n=N`
+#[post("{cabinet_id}/next")]
 async fn next(
     cabinet_id: web::Path<i32>,
-    next_n: web::Query<NextN>,
+    next_n: web::Form<NextN>,
     db_pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, Error> {
     let cabinet_id = cabinet_id.into_inner();
@@ -140,6 +141,8 @@ WHERE assoc_cabinet = $2
     Ok(HttpResponse::Ok().json(next))
 }
 
+/// Join the queue of `cabinet_id` with a name
+/// `POST /cabinets/{cabinet_id}/join - name=NAME`
 #[post("{cabinet_id}/join")]
 async fn join(
     cabinet_id: web::Path<i32>,
@@ -154,7 +157,7 @@ async fn join(
 SELECT * FROM arcqueue.players
 WHERE name = $1
 AND assoc_cabinet = $2
-",
+        ",
     )
     .bind(&name.name)
     .bind(&cabinet_id)
@@ -171,12 +174,83 @@ INSERT INTO arcqueue.players
 SELECT MAX(position) + 1, $1, $2
 FROM arcqueue.players
 WHERE assoc_cabinet = $2
-    ",
+        ",
     )
     .bind(&name.name)
     .bind(&cabinet_id)
     .execute(db_pool.get_ref())
     .await?;
+
+    Ok(HttpResponse::Ok().body("Done"))
+}
+
+/// Leave the queue of `cabinet_id` with a name
+/// `POST /cabinets/{cabinet_id}/leave - name=NAME`
+#[post("{cabinet_id}/leave")]
+async fn leave(
+    cabinet_id: web::Path<i32>,
+    name: web::Form<Name>,
+    db_pool: web::Data<PgPool>,
+) -> Result<HttpResponse, Error> {
+    let cabinet_id = cabinet_id.into_inner();
+
+    // Bails if not in the queue
+    let player: Vec<Player> = query_as(
+        "
+SELECT * FROM arcqueue.players
+WHERE name = $1
+AND assoc_cabinet = $2
+        ",
+    )
+    .bind(&name.name)
+    .bind(&cabinet_id)
+    .fetch_all(db_pool.get_ref())
+    .await?;
+
+    if player.is_empty() {
+        return Ok(HttpResponse::BadRequest().body("Player name not found in the queue"));
+    }
+
+    let mut transaction = db_pool.get_ref().begin().await?;
+
+    let (original_position,): (i32,) = query_as(
+        "
+SELECT position FROM arcqueue.players
+WHERE name = $1
+AND assoc_cabinet = $2
+        ",
+    )
+    .bind(&name.name)
+    .bind(&cabinet_id)
+    .fetch_one(&mut *transaction)
+    .await?;
+
+    query(
+        "
+DELETE FROM arcqueue.players
+WHERE name = $1
+AND assoc_cabinet = $2
+        ",
+    )
+    .bind(&name.name)
+    .bind(&cabinet_id)
+    .execute(&mut *transaction)
+    .await?;
+
+    query(
+        "
+UPDATE arcqueue.players
+SET position = position - 1
+WHERE position > $1
+AND assoc_cabinet = $2
+        ",
+    )
+    .bind(original_position)
+    .bind(&cabinet_id)
+    .execute(&mut *transaction)
+    .await?;
+
+    transaction.commit().await?;
 
     Ok(HttpResponse::Ok().body("Done"))
 }
