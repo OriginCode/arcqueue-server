d4b640d5089c07237f292a72ce95f86b6630ef1f
Kaiyang Wu <self@origincode.me>
2024-05-23 17:37:25 -0700

    chore: format schema.sql
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA arcqueue;
 
 -- Arcades Table
 CREATE TABLE arcqueue.arcades (
-	id			uuid PRIMARY KEY,
+	id		uuid PRIMARY KEY,
 	name		text NOT NULL,
 	description	text,
 	create_date	date NOT NULL
@@ -12,16 +12,16 @@ CREATE TABLE arcqueue.arcades (
 
 -- Cabinets Table
 CREATE TABLE arcqueue.cabinets (
-	id				uuid PRIMARY KEY,
-	game_name		text NOT NULL,
-	name			text NOT NULL,
+	id		uuid PRIMARY KEY,
+	game_name	text NOT NULL,
+	name		text NOT NULL,
 	assoc_arcade	uuid REFERENCES arcqueue.arcades (id)
 );
 
 -- Players Table
 CREATE TABLE arcqueue.players (
-	position		int NOT NULL,
-	name			text NOT NULL,
+	position	int NOT NULL,
+	name		text NOT NULL,
 	assoc_cabinet	uuid REFERENCES arcqueue.cabinets (id),
 	UNIQUE (position, assoc_cabinet),
 	UNIQUE (name, assoc_cabinet)
